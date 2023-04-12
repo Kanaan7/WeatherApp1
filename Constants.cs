865e0447e0a996e9a6367035397d5689e723a512
Kanaan Ziwar <k.ziwar@icloud.com>
2023-04-12 14:52:54 -0400

    Add project files.

---
+++ Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp1
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "1f6dc7dd39aa4d653da2e324e679a54e";
+    }
+}
